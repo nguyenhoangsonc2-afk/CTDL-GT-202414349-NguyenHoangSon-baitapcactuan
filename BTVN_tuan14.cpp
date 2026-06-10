b35ff2849efab941b20fa2718fe0423c5d4febd1
nguyenhoangsonc2-afk <nguyenhoangsonc2@gmail.com>
2026-06-10 15:38:47 +0700

    struct node AVL

--- BTVN_tuan14.cpp
+++ BTVN_tuan14.cpp
@@ -1,2 +1,16 @@
 #include <iostream>
 using namespace std;
+struct NodeAVL {
+    int giaTri;
+    NodeAVL *conTrai;
+    NodeAVL *conPhai;
+    int chieuCao;
+
+    // Khởi tạo một nút mới (nút lá) luôn có chiều cao bằng 0
+    NodeAVL(int x) {
+        giaTri = x;
+        conTrai = NULL;
+        conPhai = NULL;
+        chieuCao = 0; 
+    }
+};
