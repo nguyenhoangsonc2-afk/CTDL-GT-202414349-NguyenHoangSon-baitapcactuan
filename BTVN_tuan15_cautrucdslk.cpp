61ecacad67f9319caa03669367a85c924105d647
nguyenhoangsonc2-afk <nguyenhoangsonc2@gmail.com>
2026-06-16 22:20:12 +0700

    hoan thanh

--- BTVN_tuan15_cautrucdslk.cpp
+++ BTVN_tuan15_cautrucdslk.cpp
@@ -33,7 +33,7 @@ void themCanh(int u, int v) {
     chenVaoCuoi(u, v);
     chenVaoCuoi(v, u);
 }
-// duyet theo chieu rong
+// duyet theo chieu rong bfs 
 void BFS(int start) {
     bool visited[N] = {false};
     queue<int> q;
@@ -64,9 +64,11 @@ int main() {
     themCanh(0, 2);  // Hà Nội - Bắc Ninh (Đ8)
     themCanh(0, 6);  // Hà Nội - Hải Dương (Đ1)
     themCanh(0, 8);  // Hà Nội - Phủ Lý (Đ4)
+
     themCanh(0, 9);  // Hà Nội - Hòa Bình (Đ5)
     themCanh(0, 10); // Hà Nội - Sơn Tây (Đ6)
     themCanh(2, 3);  // Bắc Ninh - Bắc Giang (Đ9)
+
     themCanh(2, 4);  // Bắc Ninh - Uông Bí (Đ11)
     themCanh(3, 4);  // Bắc Giang - Uông Bí (Đ10)
     themCanh(4, 5);  // Uông Bí - Hải Phòng (Đ12)
@@ -74,7 +76,7 @@ int main() {
     themCanh(6, 7);  // Hải Dương - Hưng Yên (Đ2)
     themCanh(7, 8);  // Hưng Yên - Phủ Lý (Đ3)
 
-    cout << "Ket qua duyet BFS tu Ha Noi:\n";
+    cout << "Ket qua cua duyet BFS tu Ha Noi:\n";
     BFS(0);
 
     return 0;
