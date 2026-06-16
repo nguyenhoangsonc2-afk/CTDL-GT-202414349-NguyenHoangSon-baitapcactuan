7a036fe03158ee6f2579dbdabaf3b8b454926093
nguyenhoangsonc2-afk <nguyenhoangsonc2@gmail.com>
2026-06-16 21:38:46 +0700

    cac thanh pho

--- BTVN_tuan15_cautrucmang.cpp
+++ BTVN_tuan15_cautrucmang.cpp
@@ -1,3 +1,17 @@
 #include <iostream>
 #include <queue>
 using namespace std;
+const int N = 11;
+string tenTP[N] = {
+    "Ha Noi",
+    "Thai Nguyen",
+    "Bac Ninh",
+    "Bac Giang",
+    "Uong Bi",
+    "Hai Phong",
+    "Hai Duong",
+    "Hung Yen",
+    "Phu Ly",
+    "Hoa Binh",
+    "Son Tay"
+};
