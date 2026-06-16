a570d5a93bdc0d8e572194a7e89a963fa003c084
nguyenhoangsonc2-afk <nguyenhoangsonc2@gmail.com>
2026-06-16 22:18:59 +0700

    test

--- BTVN_tuan15_cautrucdslk.cpp
+++ BTVN_tuan15_cautrucdslk.cpp
@@ -58,3 +58,24 @@ void BFS(int start) {
     }
     cout << "END\n";
 }
+int main() {
+    
+    themCanh(0, 1);  // Hà Nội - Thái Nguyên (Đ7)
+    themCanh(0, 2);  // Hà Nội - Bắc Ninh (Đ8)
+    themCanh(0, 6);  // Hà Nội - Hải Dương (Đ1)
+    themCanh(0, 8);  // Hà Nội - Phủ Lý (Đ4)
+    themCanh(0, 9);  // Hà Nội - Hòa Bình (Đ5)
+    themCanh(0, 10); // Hà Nội - Sơn Tây (Đ6)
+    themCanh(2, 3);  // Bắc Ninh - Bắc Giang (Đ9)
+    themCanh(2, 4);  // Bắc Ninh - Uông Bí (Đ11)
+    themCanh(3, 4);  // Bắc Giang - Uông Bí (Đ10)
+    themCanh(4, 5);  // Uông Bí - Hải Phòng (Đ12)
+    themCanh(5, 6);  // Hải Phòng - Hải Dương (Đ13)
+    themCanh(6, 7);  // Hải Dương - Hưng Yên (Đ2)
+    themCanh(7, 8);  // Hưng Yên - Phủ Lý (Đ3)
+
+    cout << "Ket qua duyet BFS tu Ha Noi:\n";
+    BFS(0);
+
+    return 0;
+}
