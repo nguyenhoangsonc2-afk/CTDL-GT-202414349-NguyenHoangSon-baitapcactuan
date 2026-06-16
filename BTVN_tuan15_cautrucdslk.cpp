a24d8b5407ef44c96249ba1e6d1ef13b163e9e08
nguyenhoangsonc2-afk <nguyenhoangsonc2@gmail.com>
2026-06-16 22:13:17 +0700

    cautrucnode

--- BTVN_tuan15_cautrucdslk.cpp
+++ BTVN_tuan15_cautrucdslk.cpp
@@ -7,3 +7,8 @@ string tenTP[N] = {
     "Ha Noi", "Thai Nguyen", "Bac Ninh", "Bac Giang", "Uong Bi",
     "Hai Phong", "Hai Duong", "Hung Yen", "Phu Ly", "Hoa Binh", "Son Tay"
 };
+// cau truc node
+struct Node {
+    int dest;
+    Node* next;
+};
