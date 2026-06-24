612e2851651bdbdf898f34a0b5aa853abc724b03
nguyenhoangsonc2-afk <nguyenhoangsonc2@gmail.com>
2026-06-24 13:21:06 +0700

    cac dinh nghia

--- BTVN_tuan16.cpp
+++ BTVN_tuan16.cpp
@@ -1,3 +1,8 @@
 #include <iostream>
 #include <queue>
 using namespace std;
+const int MAX_NODES = 11;
+const int INF = 1000000;
+
+// Cấu hình cặp dữ liệu: trọng số, đỉnh đến
+typedef pair<int, int> NodePair;
