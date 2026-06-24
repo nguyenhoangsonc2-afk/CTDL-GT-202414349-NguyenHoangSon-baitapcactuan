ce126a2986c57d27de8d970642bb83d50c1a3d7a
nguyenhoangsonc2-afk <nguyenhoangsonc2@gmail.com>
2026-06-24 13:22:01 +0700

    danh sach tinh

--- BTVN_tuan16.cpp
+++ BTVN_tuan16.cpp
@@ -6,3 +6,17 @@ const int INF = 1000000;
 
 // Cấu hình cặp dữ liệu: trọng số, đỉnh đến
 typedef pair<int, int> NodePair;
+string danhSachTinh[MAX_NODES] =
+{
+    "Ha Noi",
+    "Thai Nguyen",
+    "Bac Ninh",
+    "Bac Giang",
+    "Uong Bi",
+    "Hai Phong",
+    "Hai Duong",
+    "Hung Yen",
+    "Phu Ly",
+    "Hoa Binh",
+    "Son Tay"
+};
