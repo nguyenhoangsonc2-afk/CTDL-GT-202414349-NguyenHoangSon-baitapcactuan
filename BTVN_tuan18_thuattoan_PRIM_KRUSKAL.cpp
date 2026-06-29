8c5c3f862bad6aa161d9414e67c8fb9ecdcad84c
nguyenhoangsonc2-afk <nguyenhoangsonc2@gmail.com>
2026-06-29 22:20:36 +0700

    ctruc luu tru dothi theo mtran ke

--- BTVN_tuan18_thuattoan_PRIM_KRUSKAL.cpp
+++ BTVN_tuan18_thuattoan_PRIM_KRUSKAL.cpp
@@ -6,3 +6,8 @@ using namespace std;
 const int MAX_TINH = 11;
 const int VO_CUNG = 1e9;  
 const string TEN_TINH[MAX_TINH] = { "HN", "TN", "BN", "BG", "UB", "HP", "HD", "HY", "PL", "HB", "ST" };
+// Cấu trúc lưu trữ đồ thị giao thông dưới dạng ma trận kề
+struct DoThiGiaoThong {   
+    int soDinh;
+    int maTranKe[MAX_TINH][MAX_TINH];
+};
