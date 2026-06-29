435900b9bf96cc0c32361a9f353521d77cd45f69
nguyenhoangsonc2-afk <nguyenhoangsonc2@gmail.com>
2026-06-29 22:19:28 +0700

    const

--- BTVN_tuan18_thuattoan_PRIM_KRUSKAL.cpp
+++ BTVN_tuan18_thuattoan_PRIM_KRUSKAL.cpp
@@ -3,3 +3,6 @@
 #include <queue>
 #include <iomanip>
 using namespace std;
+const int MAX_TINH = 11;
+const int VO_CUNG = 1e9;  
+const string TEN_TINH[MAX_TINH] = { "HN", "TN", "BN", "BG", "UB", "HP", "HD", "HY", "PL", "HB", "ST" };
